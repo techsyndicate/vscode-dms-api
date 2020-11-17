ea000fb5c30648f792deb2e8abf993e303b32314
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2020-11-18 01:07:32 +0530

    Contacts endpoint (#1)

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,9 +4,32 @@ const axios = require('axios')
 const { oauthHeader } = require('../controllers/constants')
 
 router.get('/', async (req, res) => {
-    let userAccessToken = req.query.access_token
-    let following = await axios.get('https://api.github.com/user/following', oauthHeader(userAccessToken)).data
-    let followers = await axios.get('https://api.github.com/user/followers', oauthHeader(userAccessToken)).data
+    const userAccessToken = req.query.access_token;
+    const following = [];
+    const followers = [];
+
+    await axios.get('https://api.github.com/user/following', oauthHeader(userAccessToken))
+      .then(response => following = response.data.forEach(value => following.push(value.login)))
+      .catch(err => console.log(err));
+    await axios.get('https://api.github.com/user/followers', oauthHeader(userAccessToken))
+      .then(response => followers = response.data.forEach(value => followers.push(value.login)))
+      .catch(err => console.log(err));
+
+    const contactsList = [];
+    const contacts = [];
+
+    following.forEach(value => {
+        followers.includes(value) ? contactsList.push(value) : null;
+    });
+    contactsList.forEach(contact => {
+        data = {
+            'username': contact,
+            'avatar_url': `https://github.com/${contact}.png`
+        }
+        contacts.push(data);
+    });
+
+    res.json(contacts);
 });
 
 module.exports = router;
