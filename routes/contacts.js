113954a15bdb3f55cbcd635c60d53cfc208565cc
oorjitchowdhary <oorjit.chowdhary@hotmail.com>
2020-11-18 00:00:48 +0530

    fix

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require('axios')
 const { oauthHeader } = require('../controllers/constants')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     let userAccessToken = req.query.access_token
     let following = await axios.get('https://api.github.com/user/following', oauthHeader(userAccessToken)).data
     let followers = await axios.get('https://api.github.com/user/followers', oauthHeader(userAccessToken)).data
