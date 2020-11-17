45bd7b9daf539515832b03f46da852318ef1b669
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-17 23:34:55 +0530

    contacts init

--- controllers/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const db = process.env.MONGODB_URL;
-
-mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 const { oauthHeader } = require('../controllers/constants')
 
 router.get('/', (req, res) => {
     let userAccessToken = req.query.access_token
-
+    let following = await axios.get('https://api.github.com/user/following', oauthHeader(userAccessToken)).data
+    let followers = await axios.get('https://api.github.com/user/followers', oauthHeader(userAccessToken)).data
 });
 
 module.exports = router;
