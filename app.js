08e788249c7e269333c5d77b320c75d58edc973c
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-17 17:22:10 +0530

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const io = require('socket.io')(5000);
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, (err) => {
+    console.log(`API listening on ${port}!`)
+    if (err) throw err
+})
+
+module.exports = app;
