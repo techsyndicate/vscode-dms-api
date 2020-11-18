72a38c068f2be69d2a63900cf203792d89bd1d5a
oorjitchowdhary <oorjit.chowdhary@hotmail.com>
2020-11-19 03:47:18 +0530

    Socket id and user endpoints

--- app.js
+++ app.js
@@ -9,47 +9,38 @@ const db = process.env.MONGODB_URL;
 
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const server = app.listen(port, (err) => {
-    console.log(`API listening on ${port}!`)
-    if (err) throw err
-})
+    console.log(`API listening on ${port}!`);
+    if (err) throw err;
+});
 
 const io = socketIO(server, { cors: true, origins: '*:*' });
 
-io.on('connection', socket => {
-    console.log('a user connected');
-})
-
 const indexRouter = require('./routes/index');
 const signinRouter = require('./routes/signin');
-const contactsRouter = require('./routes/contacts');
 const usersRouter = require('./routes/users');
-const messageRouter = require('./routes/messages')
+const messageRouter = require('./routes/messages');
 
 mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join('index.html'))
-})
 app.use('/api', indexRouter);
-app.use('/api/users/contacts', contactsRouter);
 app.use('/api/users/signin', signinRouter);
 app.use('/api/users', usersRouter);
-app.use('./api/messages', messageRouter)
+app.use('./api/messages', messageRouter);
 
 io.on('connection', socket => {
-    console.log('a user connected');
-})
+    console.log('a user connected: ' + socket.id);
+    socket.on('chat message', msg => {
+        console.log(msg);
+    });
+});
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,72 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User')
+const axios = require('axios');
+const User = require('../models/User');
+const { oauthHeader } = require('../controllers/constants')
 
 router.get('/', (req, res) => {
     let userAccessToken = req.query.access_token
     User.findOne({ access_token: userAccessToken }).then((user) => {
-        res.send(user)
-    })
+        res.send(user);
+    });
+});
+
+router.get('/contacts', async (req, res) => {
+    const userAccessToken = req.query.access_token;
+    const following = [];
+    const followers = [];
+
+    await axios.get('https://api.github.com/user/following', oauthHeader(userAccessToken))
+      .then(response => following = response.data.forEach(value => following.push(value.login)))
+      .catch(err => console.log(err));
+    await axios.get('https://api.github.com/user/followers', oauthHeader(userAccessToken))
+      .then(response => followers = response.data.forEach(value => followers.push(value.login)))
+      .catch(err => console.log(err));
+
+    const contactsList = [];
+    const contacts = [];
+
+    following.forEach(value => {
+        followers.includes(value) ? contactsList.push(value) : null;
+    });
+    contactsList.forEach(contact => {
+        data = {
+            'username': contact,
+            'avatar_url': `https://github.com/${contact}.png`
+        }
+        contacts.push(data);
+    });
+
+    res.json(contacts);
+});
+
+router.get('/socket', async (req, res) => {
+    const accessToken = req.query.access_token;
+    const socketId = req.query.socket_id;
+    const user = await User.findOne({ access_token: accessToken });
+    await user.updateOne({ socket_id: socketId });
+});
+
+router.get('/dissocket', async (req, res) => {
+    const accessToken = req.query.access_token;
+    const user = await User.findOne({ access_token: accessToken });
+    await user.updateOne({ socket_id: '' });
+});
+
+router.get('/:username/status', async (req, res) => {
+    const username = req.params.username;
+    const accessToken = req.query.access_token;
+    const getContacts = await axios.get('http://localhost:3000/api/users/contacts', { params: { access_token: accessToken }});
+    let contacts = [];
+    getContacts.data.forEach(contact => { contacts.push(contact.username); });
+
+    if (!contacts.includes(username)) {
+        res.send(401);
+    } else {
+        User.findOne({ username: username }).then(user => {
+            !user.socket_id ? res.json({ status: 'offline' }) : res.json({ status: 'online'});
+        });
+    }
 });
 
 module.exports = router;
