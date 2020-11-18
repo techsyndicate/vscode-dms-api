2c80b7f0d0b63458027f84e0b158634ddc3ccd16
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-18 15:55:15 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require("cors");
 require('dotenv').config();
 
 const db = process.env.MONGODB_URL;
@@ -16,6 +17,10 @@ const server = app.listen(port, (err) => {
 
 const io = socketIO(server);
 
+io.on('connection', socket => {
+    console.log('a user connected');
+})
+
 const indexRouter = require('./routes/index');
 const signinRouter = require('./routes/signin');
 const contactsRouter = require('./routes/contacts');
@@ -29,10 +34,14 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join('index.html'))
+})
 app.use('/api', indexRouter);
 app.use('/api/users/contacts', contactsRouter);
 app.use('/api/users/signin', signinRouter);
