476e8af225ff3a16003c9140f4f0ae5c85b92438
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-26 23:53:01 +0530

    upload.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const socketIO = require('socket.io');
 const User = require('./models/User');
 const Message = require('./models/Message')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const db = process.env.MONGODB_URL;
@@ -23,21 +24,24 @@ const indexRouter = require('./routes/index');
 const signinRouter = require('./routes/signin');
 const usersRouter = require('./routes/users');
 const messageRouter = require('./routes/messages');
+const uploadRouter = require('./routes/upload');
 
 mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 mongoose.set('useFindAndModify', false);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json(({ limit: '50mb' })));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
 
 app.use('/api', indexRouter);
 app.use('/api/users/signin', signinRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/messages', messageRouter);
+app.use('/api/upload', uploadRouter);
 
 io.on('connection', socket => {
     console.log('a user connected: ' + socket.id);
