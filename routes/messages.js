a94bbcf00288c623b16a7d6d247707c5cd026c18
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-29 02:34:30 +0530

    get Group Messages :smile:

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const axios = require('axios');
 const { oauthHeader } = require('../controllers/constants');
 const Messages = require('../models/Message');
+const User = require('../models/User');
+const e = require('express');
 
 router.get('/:username', async(req, res) => {
     const accessToken = req.query.access_token;
@@ -30,5 +32,25 @@ router.get('/:username', async(req, res) => {
         })
         .catch(err => console.log(err));
 });
+
+router.get('/group/:conversation_id', async(req, res) => {
+    const accessToken = req.query.access_token;
+    const conversation_id = req.params.conversation_id;
+    let user = await User.findOne({ access_token: accessToken })
+    if (user) {
+        Messages.find({ conversation_id: conversation_id })
+            .sort({ date: 'asc' })
+            .then(messages => {
+                if (messages.length == 0) {
+                    res.sendStatus(404)
+                } else {
+                    res.json(messages)
+                }
+            })
+            .catch(err => console.log(err));
+    } else {
+        res.sendStatus(401)
+    }
+})
 
 module.exports = router
