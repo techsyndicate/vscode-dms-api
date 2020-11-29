f1850ad3628953117ce27b99657659acbe77c6c7
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-11-29 17:59:48 +0530

    Group condition for contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -13,26 +13,30 @@ router.get('/', async(req, res) => {
         element['type'] = 'user'
         finalContacts.push(element)
     })
-    groups = user.contacts.groups
-    groups.forEach(group => {
-        finalContacts.push({
-            members: group.members,
-            name: group.name,
-            type: 'group',
-            admin: group.admin,
-            conversation_id: group.conversation_id,
-            avatar_url: group.avatar_url,
-            last_message_time: group.last_message_time,
-            last_message: group.last_message,
-            last_message_author: group.last_message_author
+    if (user.contacts.groups) {
+        groups = user.contacts.groups
+        groups.forEach(group => {
+            finalContacts.push({
+                members: group.members,
+                name: group.name,
+                type: 'group',
+                admin: group.admin,
+                conversation_id: group.conversation_id,
+                avatar_url: group.avatar_url,
+                last_message_time: group.last_message_time,
+                last_message: group.last_message,
+                last_message_author: group.last_message_author
+            })
         })
-    })
-    finalContacts.sort(function(a, b) {
-        var c = new Date(a.last_message_time);
-        var d = new Date(b.last_message_time);
-        return d - c;
-    });
-    res.send(finalContacts)
+        finalContacts.sort(function(a, b) {
+            var c = new Date(a.last_message_time);
+            var d = new Date(b.last_message_time);
+            return d - c;
+        });
+        res.send(finalContacts)
+    } else {
+        res.send(finalContacts)
+    }
 });
 
 module.exports = router
