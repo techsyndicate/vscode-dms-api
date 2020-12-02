bd83316c40a1a62e60280d0c2d06dcb3cae8c849
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-12-02 15:17:48 +0530

    :bug: fix: Authentication while viewing group messages

--- routes/messages.js
+++ routes/messages.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const { oauthHeader } = require('../controllers/constants');
 const Messages = require('../models/Message');
 const User = require('../models/User');
+const Group = require('../models/Group');
 const e = require('express');
 
 router.get('/:username', async(req, res) => {
@@ -37,7 +38,8 @@ router.get('/group/:conversation_id', async(req, res) => {
     const accessToken = req.query.access_token;
     const conversation_id = req.params.conversation_id;
     let user = await User.findOne({ access_token: accessToken })
-    if (user) {
+    let group = await Group.findOne({ conversation_id: conversation_id })
+    if (group.members.includes(user.username)) {
         Messages.find({ conversation_id: conversation_id })
             .sort({ date: 'asc' })
             .then(messages => {
